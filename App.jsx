81fc16276bff33fb4330ab4d007ec62e41557de8
vcwaleed <talhawaleed460@gmail.com>
2024-09-27 05:52:57 -0700

    updating data

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screen/HomeScreen';
+const Stack = createNativeStackNavigator();
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  );
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ src/components/TextField.jsx
@@ -0,0 +1,42 @@
+
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { FontAwesome } from 'react-native-vector-icons/FontAwesome'
+
+const TextField = ({ placeholder, value, onChangeText, iconName, iconSize = 24, iconColor = '#000', secureTextEntry }) => {
+  return (
+    <View style={styles.container}>
+      {iconName && (
+        <FontAwesome name={iconName} size={iconSize} color={iconColor} style={styles.icon} />
+      )}
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 10,
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+  },
+});
+
+export default TextField;
