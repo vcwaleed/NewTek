694bcc5ab7aae706edc27761d39af2b972aa73fb
vcwaleed <talhawaleed460@gmail.com>
2024-09-30 07:02:39 -0700

    .

--- src/screen/SignupScreen.jsx
+++ src/screen/SignupScreen.jsx
@@ -1,14 +1,26 @@
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const SignupScreen = () => {
   return (
-    <View>
-      <TouchableOpacity>
-        <Text>Hi</Text>
-      </TouchableOpacity>
-    </View>
+    <>
+      <View style={Style.container}>
+        <TouchableOpacity>
+          <Ionicons name="arrow-back" size={30} />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 };
 
 export default SignupScreen;
+
+const Style = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 60,
+    backgroundColor:'#808080',
+
+  },
+});
